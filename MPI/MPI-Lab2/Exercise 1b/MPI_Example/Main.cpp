bceece4d0c4e973d03f614e82cf3070ec74da789
Beniamin Savu <benisavu@gmail.com>
2017-03-30 11:27:01 +0300

    Added some comments

--- MPI/MPI-Lab2/Exercise 1b/MPI_Example/Main.cpp
+++ MPI/MPI-Lab2/Exercise 1b/MPI_Example/Main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 	int elementToFind = 10;
 	int i;
 
+	//text text
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
